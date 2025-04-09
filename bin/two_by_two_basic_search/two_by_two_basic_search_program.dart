39563c66fbb3cfe9d83cb640111e59f782365180
MatejGrohmann <157903399+MatejGrohmann@users.noreply.github.com>
2025-04-09 17:55:19 +0200

    feat: 2x2 3gen solution finder example

---
+++ bin/two_by_two_basic_search/two_by_two_basic_search_program.dart
@@ -0,0 +1,41 @@
+import 'package:state_solver_dart/basic_search/basic_search.dart';
+
+const uMove = [
+// 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+  [03, 00, 01, 02, 08, 09, -1, -1, 12, 13, -1, -1, 16, 17, -1, -1, 04, 05, -1, -1, -1, -1, -1, -1],
+];
+
+const rMove = [
+// 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+  [-1, 09, 10, -1, -1, -1, -1, -1, -1, 21, 22, -1, 15, 12, 13, 14, 02, -1, -1, 01, -1, 19, 16, -1],
+];
+
+const fMove = [
+// 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+  [-1, -1, 05, 06, -1, 20, 21, -1, 11, 08, 09, 10, 03, -1, -1, 02, -1, -1, -1, -1, 15, 12, -1, -1],
+];
+
+void main() {
+  final startTime = DateTime.now();
+  final movesMap = {
+    "R": rMove,
+    "U": uMove,
+    "F": fMove,
+  };
+  bool isStateSolveValidation(List<List<int>> state) {
+    final content = state[0];
+    for (int i = 0; i < content.length; i++) {
+      if (content[i] != i) return false;
+    }
+    return true;
+  }
+
+  final results = basicSearch(
+    moveDefinitions: movesMap,
+    stateValidator: isStateSolveValidation,
+    maxDepth: 10,
+    scramble: "R U R' U' R' F R F'",
+  );
+
+  print('finished in ${DateTime.now().difference(startTime)}');
+}
